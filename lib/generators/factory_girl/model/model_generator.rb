6a7b68471ca45405ef6f8f1e0349bedcd53c803c
Jessie A. Young <jessie.a.young@gmail.com>
2014-07-25 16:10:08 -0700

    Do not generate new file if `factories.rb` exists
    * WIP - this does not generate new file, but does not add factory to
      `factories.rb` yet
    * Reference: https://github.com/thoughtbot/suspenders/pull/339

--- lib/generators/factory_girl/model/model_generator.rb
+++ lib/generators/factory_girl/model/model_generator.rb
@@ -8,8 +8,18 @@ module FactoryGirl
       class_option :dir, :type => :string, :default => "test/factories", :desc => "The directory where the factories should go"
 
       def create_fixture_file
-        filename = [table_name, filename_suffix].compact.join('_')
-        template 'fixtures.erb', File.join(options[:dir], "#{filename}.rb")
+        factories_file = options[:dir] + ".rb"
+
+        if File.exist?(factories_file)
+          factory_definition = template 'single_file_fixtures.erb'
+
+          text = File.read(factories_file)
+          new_text = text.gsub(/^end/, factory_definition)
+          File.open(factories_file, "w") {|f| f.puts new_text}
+        else
+          filename = [table_name, filename_suffix].compact.join('_')
+          template 'fixtures.erb', File.join(options[:dir], "#{filename}.rb")
+        end
       end
 
       private
@@ -18,13 +28,13 @@ module FactoryGirl
         factory_girl_options[:suffix]
       end
 
+      def factory_girl_options
+        generators.options[:factory_girl] || {}
+      end
+
       def generators
         config = FactoryGirl::Railtie.config
         config.respond_to?(:app_generators) ? config.app_generators : config.generators
-      end
-
-      def factory_girl_options
-        generators.options[:factory_girl] || {}
       end
     end
   end
